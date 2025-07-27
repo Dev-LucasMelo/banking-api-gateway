448ab6d199e674a93173a4ef257fe7b483bd16ba
Lucas Melo <lmelo072002@gmail.com>
2025-07-27 01:07:14 -0300

    feature/comunicacaoHTTP: configurando nest para se comunicar com micro serviços da mesma rede docker e ajustes no docker compose para capturar o arquivo .env do diretorio

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RedisModule],
+  imports: [
+    RedisModule,
+    HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
