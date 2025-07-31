0b113e112409395d4190f04b8143092a4895a4ff
Lucas Melo <lmelo072002@gmail.com>
2025-07-31 20:30:00 -0300

    feature/updateProfilePicture: Implementando interface que recebe arquivo e simula upload para o S3

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { clientLoggedIn } from 'src/auth/decorators/clientLoggedIn.decorator';
 import { JWTguard } from 'src/auth/jwt.guard';
 import { RedisService } from 'src/redis/redis.service';
 import { ClientService } from './client.service';
 import { updateClientDto } from './validation/updateClientDto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express'
 
 @UseGuards(JWTguard)
 @Controller('client')
@@ -23,5 +25,17 @@ export class ClientController {
     async update(@clientLoggedIn() client: any, @Body() data: updateClientDto) {
         return await this.service.update(client.id, data)
     }
+
+    @Patch('/profile-picture')
+    @UseInterceptors(FileInterceptor('file'))
+    
+    async uploadProfilePicture(
+        @UploadedFile() file: Express.Multer.File, 
+        @clientLoggedIn() client: any
+    ) {
+        return await this.service.updateProfilePicture(file, client.id)
+    }
+
+
 
 }
--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -38,4 +38,28 @@ export class ClientService {
         });
     }
 
+    async uploadS3(file: Express.Multer.File, clientId: string): Promise<string> {
+        const { originalname } = file
+        return `uploads/${clientId}/${originalname.replace(/[. ]/g, '_')}`
+    }
+
+    async updateProfilePicture(file: Express.Multer.File, id: string) {
+        let urlS3 = await this.uploadS3(file, id)
+
+        const data = {
+            urlS3
+        }
+
+        return await firstValueFrom(
+            this.httpService.patch(`${this.BaseUrlClient}client/${id}/profile-picture`, data)
+        ).then((response) => {
+            return response.data;
+        }).catch((err) => {
+            const { response } = err
+            const { data } = response
+
+            throw new BadRequestException(data);
+        });
+    }
+
 }
